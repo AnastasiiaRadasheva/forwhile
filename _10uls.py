881cc401f222ee952a284155e8b318959242bc2e
Настя Радашева <eha20082@gmail.com>
2025-02-24 13:27:00 +0200

    nenene

--- _10uls.py
+++ _10uls.py
@@ -1,3 +1,86 @@
+#27
+for A in range(10, 100):
+    for B in range(10, 100):
+        num1 = int(str(A) + str(B))
+        num2 = int(str(B) + str(A)) 
+        
+        if num1 % 99 == 0 and num2 % 49 == 0:
+            print(f"A = {A}, B = {B}")
+
+#13
+count = 0
+total_sum = 0
+
+for i in range(100, 1001):
+    if i % 7 == 0:
+        count += 1
+        total_sum += i
+print(f"Seitsmega jaguvate arvude arv: {count}")
+print(f"Seitsmega jaguvate arvude summa: {total_sum}")
+#7
+veedi=int(input('sisesta K: '))
+A=int(input('sisesta A: '))
+B=int(input('sisesta B: '))
+for i in range(A, B + 1):
+    if i % veedi == 0:
+        print(i)
+#8
+print('дюймы   см')
+for i in range(1, 21):
+    print(i,"     ", i * 2.5)
+
+#9
+euro = int(input("Sisesta eurode arv: "))
+aasta = int(input("Sisesta aasta: "))
+while aasta > 0:
+    euro = (euro * 3)/100 + euro
+    aasta -= 1
+    print (round(euro, 2))
+31
+K = int(input("Sisesta kotlettide arv: "))
+M = int(input("Sisesta kotlettide arv ühes pannil: "))
+
+pants = K // M
+remaining_cutlets = K % M
+
+print(f"Täis panne: {pants}")
+print(f"Viimasele pannile jäävad kotletid: {remaining_cutlets}")
+#16
+for i in range(1, 10):
+    print("0" * (i - 1) + str(i) + "0" * (9 - i))
+
+#21
+numbrid = [abs(int(input(f"Sisesta arv {i+1}: "))) for i in range(10)]
+
+print("Muudetud arvud:", numbrid)
+
+#14
+import random
+numeeq=random.randint(1,100)
+pip=1
+while pip<=numeeq:
+    try:
+        ymnoj=pip*numeeq
+        pip+=1
+        print(f'{pip-1} * {numeeq} = {ymnoj}')
+    except:
+        break
+#28 import random
+num=random.randint(1,100)
+while True:
+    try:
+        polz1=int(input('vvedite chislo: '))
+
+        if num>polz1:
+            print('chislo bolshe')
+        elif num<polz1:
+            print('chislo menshe')
+        else: 
+            print('vi ugadali!!!!')
+            break
+    except:
+        print('nepravilnoe znachenie')
+
 #18
 for i in range(20, 51):
     if i % 3 == 0 and i % 5 != 0:
@@ -28,9 +111,6 @@ for i in range(1,10):
 print()
 
 #15
-from difflib import SequenceMatcher
-
-
 for j in range(10):
     for i in range(10):
         print(i,end=' ')
@@ -113,4 +193,3 @@ while True:
             print('arv a üeab alema rohkem kui 1')
     except ValueError:
         print('bbebebebe')
-
